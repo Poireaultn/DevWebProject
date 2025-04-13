1e884256878eaa1171f9b3cd7dea45de75ccc030
Remi <reminous32@gmail.com>
2025-04-13 16:03:14 +0200

    feat: Changement du nom de l'application de 'Mon Site' à 'CY Tech'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Mon Site')</title>
+    <title>@yield('title', 'CY Tech')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
@@ -11,9 +11,9 @@
     <nav class="navbar navbar-expand-lg">
         <div class="container">
             @auth
-                <a class="navbar-brand" href="/">Mon Site</a>
+                <a class="navbar-brand" href="/">CY Tech</a>
             @else
-                <a class="navbar-brand" href="/information">Mon Site</a>
+                <a class="navbar-brand" href="/information">CY Tech</a>
             @endauth
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
